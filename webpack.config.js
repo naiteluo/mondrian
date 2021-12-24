98560ab3e60421647d9228638a2d0571f0ca7c6f
mob <tanjiawei@zuoyebang.com>
2021-12-24 17:29:45 +0800

    add contenthash to output file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/app.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "app.js",
+    filename: "[name].[contenthash].js",
   },
 
   module: {
