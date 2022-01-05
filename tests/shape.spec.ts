f6e4eeb1c4444fd98074a77bfe107d10eb4b9034
mob <tanjiawei@zuoyebang.com>
2022-01-05 19:35:03 +0800

    add triangle test

--- tests/shape.spec.ts
+++ tests/shape.spec.ts
@@ -85,4 +85,28 @@ test.describe("shape", () => {
     await mp.hideUI();
     await mp.screenshotAndCompare();
   });
+
+  test("triangle", async ({ page }, testInfo) => {
+    const mp = new MondrianPage(page, testInfo);
+    await mp.init();
+
+    await page.selectOption('[data-test-id="brushName"] select', "Triangle");
+
+    for (let i = 0; i < basicShapeData.length; i++) {
+      const lineData = basicShapeData[i];
+      for (let j = 0; j < lineData.length; j++) {
+        const [x, y] = lineData[j];
+        await mp.page.mouse.move(x, y);
+        if (j === 0) {
+          await mp.page.mouse.down();
+        }
+        if (j === lineData.length - 1) {
+          await mp.page.mouse.up();
+        }
+      }
+    }
+
+    await mp.hideUI();
+    await mp.screenshotAndCompare();
+  });
 });
